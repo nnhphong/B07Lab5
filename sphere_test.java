f090a820b2a2639c725c8d3b0a2f214a31ce4093
nnhoangphong <nnhoangphong@gmail.com>
2024-06-27 13:05:38 -0400

    readded sphere_test.java

---
+++ sphere_test.java
@@ -0,0 +1,19 @@
+package B07Lab5.B07Lab5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class sphere_test {
+    @Test
+    void testGetVolume() {
+        Sphere s = new Sphere(5);
+        assertTrue(Math.abs(s.getVolume() - 523.598) < 0.01);
+    }
+    
+    @Test
+    void testGetSurfaceArea() {
+        Sphere s = new Sphere(5);
+        assertTrue(Math.abs(s.getVolume() - 314.159) < 0.01);
+    }
+}
